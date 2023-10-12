f335f3467ee9d5f062b9cf8a07c9a2ef633c57bd
Balamurugan R <balamuruganrdckap@gmail.com>
2023-10-12 16:18:11 +0530

    assign functionality almost completed in both side

--- frontend/src/components/AdminAndManger/UserListByManger.jsx
+++ frontend/src/components/AdminAndManger/UserListByManger.jsx
@@ -1,51 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import axios from 'axios';
-// import Item from 'antd/es/list/Item';
 
 export default function UserListByManger() {
-
     const [userList, setUsers] = useState([]);
     const [setId, setBothId] = useState([]);
-    const [managerId, setManagerId] = useState('')
 
-    const [exits, setExits] = useState({});
-
-    // const [buttonText, setButtonText] = useState('Unassigned');
-
-    // const [assign, setAssign] = useState([])
     const { id } = useParams();
 
     useEffect(() => {
         axios.get(`http://localhost:5051/usersList/${id}`)
             .then(res => {
-                console.log(res)
-                // setManagerId(id)
-                setUsers(res.data.right)
+                
+                setUsers(res.data.data)
+                setBothId(res.data.right)
             })
             .catch(err => console.log(err))
-    }, [])
+    }, []);
 
+    // console.log("setId", setId);
+    // console.log("managerIdm----", id);
+
+    // let users = setId.filter(ele => ele);
+    // console.log(users);
+
+    //id.user_id == 1 ? "already assigned" : "newly assign"
+
+
+    // useEffect(() => {
     const handleUserAssign = (e) => {
         const payload = {
-            managerId: managerId,
+            managerId: id,
             userId: e.target.id
         }
         axios.post('http://localhost:5051/adminHome/managerList', payload)
             .then(res => {
-                console.log(res);
-
                 if (res.data.Status === "Success") {
                     alert("User assigned successfully")
-                    // window.location.reload(true)
+                    window.location.reload(true)
                 }
                 else {
-
                     alert(res.data.Error)
-                    // let exitsId = {};
-                    // exitsId.userId = res.data.userId;
-
-                    // setExits(exitsId)
                 }
 
             })
@@ -53,8 +48,11 @@ export default function UserListByManger() {
                 alert("Can not assign the users")
             })
     }
+    // handleUserAssign()
+    // }, [])
 
-
+    let idx = [];
+    setId.filter(id => idx.push(id.user_id));
     return (
         <React.Fragment>
             <div>
@@ -70,22 +68,25 @@ export default function UserListByManger() {
                                 <th scope="col">User Name</th>
                                 <th scope="col">User Email</th>
                                 <th>Assign to</th>
-                                {/* <th>id</th> */}
                             </tr>
                         </thead>
                         <tbody className="userList">
-                            {userList.map((user, index) => {
 
+                            {userList.map((user, index) => {
                                 return (
                                     <tr key={index}>
                                         <th scope="row">{user.id}</th>
                                         <td>{user.user_name}</td>
                                         <td>{user.email}</td>
-
-                                        <td><input type='button' value="Assign" id={user.id} onClick={handleUserAssign}></input></td>
-
+                                        <td><input type='button' className={idx.filter(id => id == user.id) == user.id ? "btn btn-outline-success btn-sm" : "btn btn-outline-danger btn-sm"} value={idx.filter(id => id == user.id) == user.id ? "Assigned" : "Unassgined"} id={user.id} onClick={handleUserAssign}></input></td>
                                     </tr>)
                             })}
+                            {/* {
+                                setId.map((item, i) => {
+                                    // console.log("itemasdas", item.user_id);
+                                    setfinalId(item.user_id);
+                                })
+                            } */}
                         </tbody>
                     </table>
                 </div>
