09385ea20328236a2a312434034e02c486e2da68
Balamurugan R <balamuruganrdckap@gmail.com>
2023-10-14 10:37:35 +0530

    just correction in the task add functionality at both side

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import _ from 'lodash';
+import bodyParser from "body-parser"
 
-// import bodyParser from "body-parser"
 // import { LocalStorage } from "node-localstorage";
 // import localStorage from 'node-localstorage';
 // localStorage = new localStorage();
@@ -25,8 +25,10 @@ app.use(cors({
     methods: ["POST", "GET"],
     credentials: true
 }));
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 // app.use(cookieParser());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 
 
@@ -342,6 +344,7 @@ app.post('/adminOrManagerLogin', (req, res) => {
 
 
 app.post('/userHome', verifyUser, (req, res) => {
+    console.log(req.body);
     const sql = "INSERT INTO userTasks (`task_name`,`description`,`user_id`) VALUES(?)";
     const values = [
         req.body.taskName,
